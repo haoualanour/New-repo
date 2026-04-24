4f3c210543aacf1c560514669040ceb932f44379
haoualanour <haoualanour0@gmail.com>
2026-04-24 10:22:47 +0100

    Update projet_sec_media.py

--- projet_sec_media.py
+++ projet_sec_media.py
@@ -621,7 +621,18 @@ class WatermarkApp(tk.Tk):
 
 
 # ══════════════════════════════════════════════════════════════════════════════
+#if __name__ == "__main__":
+#    print("version 2")
+#    app = WatermarkApp()
+#    app.mainloop()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Mon application fonctionne sur Azure 🚀"
+
 if __name__ == "__main__":
-    print("version 2")
-    app = WatermarkApp()
-    app.mainloop()
+    app.run(host="0.0.0.0", port=80)
